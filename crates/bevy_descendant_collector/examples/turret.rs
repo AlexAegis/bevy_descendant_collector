ad7d7ba4e73f38f74e2b78fccd43289e2af54979
AlexAegis <alexaegis@pm.me>
2025-03-27 21:56:57 +0100

    remove bevy_asset_loader

--- crates/bevy_descendant_collector/examples/turret.rs
+++ crates/bevy_descendant_collector/examples/turret.rs
@@ -1,17 +1,8 @@
 use bevy::prelude::*;
-use bevy_asset_loader::asset_collection::AssetCollection;
-use bevy_asset_loader::loading_state::config::ConfigureLoadingState;
-use bevy_asset_loader::loading_state::{LoadingState, LoadingStateAppExt};
 use bevy_descendant_collector::*;
 use bevy_inspector_egui::inspector_options::ReflectInspectorOptions;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_inspector_egui::InspectorOptions;
-
-#[derive(Default, AssetCollection, Resource)]
-pub struct TurretModelAssets {
-	#[asset(path = "models/simple_turret.glb#Scene0")]
-	pub turret_model: Handle<Scene>,
-}
 
 /// This struct will be populated from a loaded gltf scene, based on name paths.
 #[derive(Component, EntityCollectorTarget, Reflect, InspectorOptions)]
@@ -31,13 +22,6 @@ pub struct MyTurretArmature {
 	pub head_ik_ctrl: Entity,
 }
 
-#[derive(States, Default, Debug, Hash, PartialEq, PartialOrd, Eq, Clone, Copy)]
-enum ExampleAppState {
-	#[default]
-	Loading,
-	Loaded,
-}
-
 fn main() -> AppExit {
 	App::new()
 		.add_plugins((
@@ -45,26 +29,19 @@ fn main() -> AppExit {
 			WorldInspectorPlugin::new(),
 			DescendantCollectorPlugin::<MyTurretArmature>::new(HierarchyRootPosition::Scene),
 		))
-		.init_state::<ExampleAppState>()
 		.register_type::<MyTurretArmature>()
-		.add_loading_state(
-			LoadingState::new(ExampleAppState::Loading)
-				.load_collection::<TurretModelAssets>()
-				.continue_to_state(ExampleAppState::Loaded),
-		)
-		.add_systems(Startup, spawn_example_scene)
-		.add_systems(OnEnter(ExampleAppState::Loaded), spawn_turret)
+		.add_systems(Startup, setup)
 		.run()
 }
 
-fn spawn_turret(mut commands: Commands, turret_model_assets: Res<TurretModelAssets>) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 	commands.spawn((
-		SceneRoot(turret_model_assets.turret_model.clone()),
+		SceneRoot(
+			asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/simple_turret.glb")),
+		),
 		DescendantCollectorTarget::<MyTurretArmature>::default(), // marking this entity that it needs an accumulator
 	));
-}
 
-fn spawn_example_scene(mut commands: Commands) {
 	commands.spawn((
 		Camera3d::default(),
 		Transform::from_xyz(1., 4., 5.).looking_at(Vec3::ZERO, Vec3::Y),
